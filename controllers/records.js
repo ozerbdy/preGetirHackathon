0d8408de997a11cd5ab31b6f75e5e116e529da78
Özer Baday <ozer.baday@gmail.com>
2018-01-28 21:41:03 +0300

    Validate request object parameters and respond accordingly

--- controllers/records.js
+++ controllers/records.js
@@ -1,5 +1,33 @@
-const RecordsRepository = require('../db/RecordsRepository');
+const RecordsRepository = require('../db/RecordsRepository'),
+    ResponseHelpers = require('../helpers/responseHelpers'),
+    Constants = require('../constants/constants'),
+    Joi = require('joi');
 
+function getResponseModel(responseStatusObject, records){
+    responseStatusObject.records = records || [];
+    return responseStatusObject;
+}
+
+module.exports.validateSeachRecordsRequest = function(req, res, next) {
+    const searchRecordsRequestSchema = {
+        startDate: Joi.date().iso().required(),
+        endDate: Joi.date().iso().required(),
+        minCount: Joi.number().integer().required(),
+        maxCount: Joi.number().integer().required(),
+    };
+    const result = Joi.validate(req.body, searchRecordsRequestSchema, {
+        allowUnknown: true,
+        // return all errors a payload contains, not just the first one Joi finds
+        abortEarly: false
+    });
+    if(result.error){
+        const basicResponseObject = ResponseHelpers.getBasicResponseObject(Constants.ErrorInfo.RequestBodyValidationFailed, result.error.toString());
+        const finalResponseObject = getResponseModel(basicResponseObject);
+        return res.status(400).send(finalResponseObject);
+    }else{
+        return next();
+    }
+};
 
 module.exports.searchRecords = function(req, res){
     const startDate =  new Date(req.body.startDate);
@@ -8,10 +36,10 @@ module.exports.searchRecords = function(req, res){
     const minCount = req.body.minCount;
 
     RecordsRepository.getRecords(startDate, endDate, minCount, maxCount, (err, records) => {
-        return res.send({
-            success: !err,
-            err: err,
-            records: records
-        });
+        if (err) return res.send(getResponseModel(ResponseHelpers.getBasicResponseObject(Constants.ErrorInfo.MongoError)));
+
+        const basicResponseObject = ResponseHelpers.getBasicResponseObject(Constants.SuccessInfo);
+        const finalResponseObject = getResponseModel(basicResponseObject, records);
+        return res.send(finalResponseObject);
     });
 };
--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require('express'),
     Router = express.Router(),
     RecordController = require('./controllers/records');
 
-Router.post('/searchRecord', RecordController.searchRecords);
+Router.post('/searchRecord', RecordController.validateSeachRecordsRequest, RecordController.searchRecords);
 
 module.exports = Router;
 
